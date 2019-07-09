8f0a7e20f96ce2103156f36d46636af24cc140f1
dajun <rcdchen@qq.com>
2019-07-09 19:02:43 +0800

    the 05 Instance is finished.

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,8 @@ public:
 
 private:
     GLFWwindow* window;
-    
+    VkInstance instance;
+
     void initWindow(){
 		glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);  // GLFW最初是被设计为创建OpenGL上下文而用的，我们需要在接下来的函数调用中告诉它不要创建OpenGL上下文
@@ -33,6 +34,7 @@ private:
     }
 
     void initVulkan() {
+        createInstance();
 
     }
 
@@ -44,14 +46,44 @@ private:
     }
 
     void cleanup() {
+		vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);  // 一旦窗口被关闭，我们需要销毁资源，关闭GLFW.
-        
+
         glfwTerminate();
 
     }
 
- 
-    
+    void createInstance() {
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo = {};  // 主要用来声明我们想要使用哪些全局扩展和验证层。这里的全局是指整个程序，不是某个device。
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+		// Vulkan是平台无关的，它不能直接与平台窗体系统通信，为了连接Vulkan和窗体系统,使得被渲染后的结果显示到屏幕上，
+		// 我们需要使用WSI扩展(Window System Integration extensions), 这里我们将使用VK_KHR_surface。
+        // VK_KHR_surface扩展是一个instance级的扩展，它包含在glfwGetRequiredInstanceExtensions返回的列表中。该列表还包括一些其他WSI扩展.
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount); // 参数glfwExtensionCount返回实例扩展的数量。函数返回值glfwExtensions是实例扩展的名称数组。
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;   // 我们暂时不需要 验证层。
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+
 };
 
 int main() {
